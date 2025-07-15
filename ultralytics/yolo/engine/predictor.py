9e9fec8f89f51494cf0e2987c46eaadda9ad4fc6
Frank <54981806+LAKERFrank@users.noreply.github.com>
2025-07-15 16:39:46 +0800

    Handle grayscale images in predictor

--- ultralytics/yolo/engine/predictor.py
+++ ultralytics/yolo/engine/predictor.py
@@ -132,7 +132,12 @@ class BasePredictor:
         not_tensor = not isinstance(im, torch.Tensor)
         if not_tensor:
             im = np.stack(self.pre_transform(im))
-            im = im[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW, (n, 3, h, w)
+            if im.ndim == 3:
+                im = im[:, None, :, :]
+            elif im.shape[-1] == 1:
+                im = im.transpose((0, 3, 1, 2))
+            else:
+                im = im[..., ::-1].transpose((0, 3, 1, 2))  # BGR to RGB, BHWC to BCHW, (n, 3, h, w)
             im = np.ascontiguousarray(im)  # contiguous
             im = torch.from_numpy(im)
 
